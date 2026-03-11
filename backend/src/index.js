50e3aa18f9544bd8fd573c40befc78b7f0e6da3b
Adonay Flores <105473836+AdonayFlores@users.noreply.github.com>
2026-03-10 21:19:37 -0600

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -21,7 +21,8 @@ const app = express();
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "https://tu-app.vercel.app"
+    "http://localhost:3000",
+    "https://clubtimoteo.vercel.app"
   ],
   credentials: true
 }));
