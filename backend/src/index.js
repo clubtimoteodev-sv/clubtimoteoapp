c45879bef1de78186213addfda587cc316f18337
endercraft95 <endercraft95@EnderDesktop.enderdesktop.com>
2026-04-03 12:37:07 -0600

    Fix: Implementar MultiDestacamenoto y dominio personalizado2

--- backend/src/index.js
+++ backend/src/index.js
@@ -23,7 +23,7 @@ app.use(cors({
   origin: function (origin, callback) {
     const allowedOrigins = [
       "http://localhost:5173",
-      "http://localhost:3000", "
+      "http://localhost:3000", 
       "https://clubtimoteo.vercel.app",
       "https://www.clubtimoteo.com"
     ];
